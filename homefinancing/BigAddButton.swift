f0259140dbdc1cb2e8c88d30437dc718ccb9eff0
宫辰 <chen.gong@ctgtmo.com>
2016-04-06 17:49:53 +0800

    home

--- homefinancing/BigAddButton.swift
+++ homefinancing/BigAddButton.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class BigAddButton: UIButton {
     
+    let buttonWidth:CGFloat = 94
+    
+    var plusLabel:UILabel!
+    var addLabel:UILabel!
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        var plusLabel:UILabel
-        var addOneLabel:UILabel
+        plusLabel = UILabel(frame:CGRectMake(0,0,buttonWidth,35))
+        plusLabel.textAlignment = NSTextAlignment.Center
+        plusLabel.font = UIFont.boldSystemFontOfSize(30)
+        plusLabel.textColor = UIColor.whiteColor()
+        plusLabel.text = "+"
+        self.addSubview(plusLabel)
+        
+        addLabel = UILabel(frame:CGRectMake(0,20,buttonWidth,40))
+        addLabel.textAlignment = NSTextAlignment.Center
+        addLabel.font = UIFont.systemFontOfSize(16)
+        addLabel.textColor = UIColor.whiteColor()
+        addLabel.text = "记一笔"
+        self.addSubview(addLabel)
     }
+    
+    
 }
--- homefinancing/HomeViewController.swift
+++ homefinancing/HomeViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class HomeViewController: HFBaseViewController {
+class HomeViewController: HFBaseViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: HFBaseTableView!
+    @IBOutlet weak var payLabel: UILabel!
+    @IBOutlet weak var incomeLabel: UILabel!
+    @IBOutlet weak var topCenterView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.navigationController?.navigationBar.barTintColor = appPayColor;
+        self.navigationController?.navigationBar.barTintColor = appPayColor
+        payLabel.adjustsFontSizeToFitWidth = true;
+        incomeLabel.adjustsFontSizeToFitWidth = true;
         
+        let progress = KDCircularProgress(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        progress.startAngle = -90
+        progress.progressThickness = 0.35
+        progress.trackThickness = 0.35
+        progress.clockwise = false
+        progress.gradientRotateSpeed = 2
+        progress.roundedCorners = true
+        progress.glowMode = .NoGlow
+        progress.angle = 275
+        progress.setColors(appIncomeColor)
+        progress.trackColor = appLitePayColor
+        topCenterView.addSubview(progress)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - TableView
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    //每一块有多少行
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //绘制cell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let initIdentifier = "Cell"
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: initIdentifier)
+        
+        cell.accessoryType = UITableViewCellAccessoryType.None
+        //下面两个属性对应subtitle
+        cell.textLabel?.text = "测试"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    //每个cell的高度
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
     }
     
     
@@ -31,5 +79,8 @@ class HomeViewController: HFBaseViewController {
         // Pass the selected object to the new view controller.
     }
     
+    @IBAction func addClickAction(sender: AnyObject) {
+        
+    }
 
 }
